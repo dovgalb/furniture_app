2681b925013a16bbd78535010f904725cc89d62a
dovgalb <bogdandovgal1208@gmail.com>
2024-01-09 18:08:41 +0300

    добавил модели в  orm

--- adapters/orm.py
+++ adapters/orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy import MetaData, Table, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import mapper
 
 from domain import model
@@ -12,6 +12,33 @@ order_lines = Table('order_lines', metadata,
                     Column('orderid', String(255)),
                     )
 
+batches = Table(
+    "batches",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column('reference', String(255)),
+    Column('sku', String(255)),
+    Column('_purchased_quantity', Integer, nullable=False),
+    Column('eta', Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('orderline_id', ForeignKey("order_lines.id")),
+    Column('batch_id', ForeignKey("batches.id")),
+)
+
 
 def start_mappers():
     lines_mapper = mapper(model.OrderLine, order_lines)
+    mapper(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper, secondary=allocations, collection_class=set,
+            )
+        },
+    )
